01129618ecc40f9e2f8b6e81436e88ad923a1571
HimeLiv <liviodelenclos19@gmail.com>
2025-01-23 10:43:12 +0100

    Test truite main x2

--- src/com/afpa/www/main/Main.java
+++ src/com/afpa/www/main/Main.java
@@ -7,6 +7,7 @@ import com.afpa.www.exception.CreationImpossible;
 public class Main {
     public static void main(String[] args) throws CreationImpossible {
         System.out.println("Bonjour");
+        Truite truite = new Truite(5,5); 
         Truite truite = new Truite(5,5);
         System.out.println(truite);
         System.out.println(truite);
